975207378290f1e8feb7af4d5c16c9c0b40235a5
Ragil09 <149141413+Ragil09@users.noreply.github.com>
2025-04-09 11:58:50 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,6 @@ use App\Http\Controllers\AuthController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::get('/test', function () {
+    return response()->json(['message' => 'Laravel API OK']);
+});
